d9ba0415cdbaefa5d640ea90960b3e9e99046222
Anirban chattaraj <anirbanchattaraj@Anirbans-MacBook-Air.local>
2026-03-15 21:20:51 +0530

    Add rotate array using reversal algorithm

---
+++ 59.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void reverse(int arr[], int a, int b){
+    for(int i=a, j=b; i<j; i++, j--){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+int main(){
+    int n;
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    int arr[n];
+    int k;
+    printf("Enter array elements:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter k: ");
+    scanf("%d",&k);
+    k = k % n;
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+    printf("Rotated array:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
